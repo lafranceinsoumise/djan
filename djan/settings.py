4d13076d1a40a297f09cdc7b37fbbc5bd68d12f3
Jill Royer <hello@jillroyer.me>
2020-05-28 17:48:27 +0200

    feat: secret_key and debug env

--- djan/settings.py
+++ djan/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+z-b4g@w(hz7f4(hjrg8x-+%818&ixz*n7ybkppw1$*+xzetbt"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "true").lower() == "true"
 
 ALLOWED_HOSTS = []
 
